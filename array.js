ccea76ca22f600e613259bc90c4a877b1b59ecea
paulolucasz <paulolucas.5000@gmail.com>
2025-09-02 15:58:46 -0300

    terceiro commit

--- array.js
+++ array.js
@@ -1,9 +1,11 @@
 const notas = [5 , 10 ,15]
 console.log(notas);
 notas.push (41);
+console.log(notas);
 notas [14] = 20;
 console.log(notas.reverse())
 
-notas.forEach( Element => {
-    console.log(`Elemento e ${Element}`)
-})
+// notas.forEach( Element => {
+   // console.log(`Elemento e ${Element}`)
+// }) 
+
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const prompt = require ('prompt-sync')();
+const prompt = require('prompt-sync')();
 
-function perguntaIdade () {
-	const idade = prompt('Qual é a idade? '); 
-	return idade;
+// function perguntaIdade () {
+// 	const idade = prompt('Qual é a idade? '); 
+// 	return idade;
+// }
+
+// const idadeDoAnimal = perguntaIdade();
+
+// console.log(`A idade do animal é: ${idadeDoAnimal} anos`);
+
+// const calcularIdadeAjustada = (idadeAntig) => {
+//     const idadeVerdadeira = idadeAntig * 7;
+//     return idadeVerdadeira;
+
+// }
+
+// const resultado = calcularIdadeAjustada(idadeDoAnimal)
+
+// console.log(resultado)
+
+let gatos = [];
+
+function cadastrarGatos() {
+    let quantidadeDeGatos = prompt('Quantos gatos você tem? ');
+    let numerosDeGatos = parseInt(quantidadeDeGatos);
+
+    for (let i = 0; i < numerosDeGatos; i++) {
+        gatos[i] = prompt(`Qual é a idade do gatinho ${i + 1}: `);
+    }
+
+    gatos.forEach((gato, indice) => {
+        console.log(`A idade do gatinho ${indice + 1} é ${gato}`);
+    });
 }
 
-const idadeDoAnimal = perguntaIdade();
-
-console.log(`A idade do animal é: ${idadeDoAnimal} anos`);
-
-const calcularIdadeAjustada = (idadeAntig) => {
-    const idadeVerdadeira = idadeAntig * 7;
-    return idadeVerdadeira;
-
-}
-
-const resultado = calcularIdadeAjustada(idadeDoAnimal)
-
-console.log(resultado)
-
+cadastrarGatos();
