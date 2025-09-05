2d58e249ac4a37049968fcf4ad7f608780d7500f
paulolucasz <paulolucas.5000@gmail.com>
2025-09-05 14:32:28 -0300

    add teste hoje

---
+++ exemplo.js
@@ -0,0 +1,13 @@
+const animal = {
+    nome: "Bruno",
+    especie: "Papagaio",
+    raca: "Rabugento",
+    idade: "75",  
+
+}
+
+console.log(animal);
+
+animal.nome = "BBBBB";
+
+console.log(animal);
