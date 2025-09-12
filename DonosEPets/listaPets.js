641d2c5168cea04b1e1304698085b29bceaf23a2
paulolucasz <paulolucas.5000@gmail.com>
2025-09-12 15:52:11 -0300

    atualizando as pastas novo trampo

---
+++ DonosEPets/listaPets.js
@@ -0,0 +1,19 @@
+const pets = [
+    { nome: "Fifi", idade: 12, especie:"Cachorro",raca:"Labrador"},
+    { nome: "Bugaosh", idade: 2, especie:"Cachorro",raca:"SRD"},
+    { nome: "Lihasd", idade: 3, especie:"Gato",raca:"Frajola"}
+]
+
+const container = document.getElementById("lista");
+
+pets.forEach(pet => {
+    const newEl = document.createElement("div");
+    container.appendChild(newEl);
+    const newNome = document.createElement("h3");
+    const newIdade = document.createElement("h3");
+    newEl.appendChild(newNome);
+    newEl.appendChild(newIdade);
+    newNome.innerText= pet.nome;
+    newIdade.innerText = pet.idade;
+    newEl.style.border = "black solid 2px";
+})
